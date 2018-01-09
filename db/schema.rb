ce72f0ac46c839cf86f2fcdfcb301b33ed934bfb
Andrew <rodandrew95@gmail.com>
2018-01-09 14:01:43 -0800

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,57 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925112539) do
+ActiveRecord::Schema.define(version: 20180109215902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "notes", force: :cascade do |t|
-    t.bigint "resource_id"
-    t.string "name"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["resource_id"], name: "index_notes_on_resource_id"
-  end
-
-  create_table "resources", force: :cascade do |t|
-    t.bigint "skill_id"
-    t.string "name"
-    t.string "url"
-    t.string "status"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "notes_count", default: 0
-    t.index ["skill_id"], name: "index_resources_on_skill_id"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.integer "parent_id"
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
-    t.integer "resources_count", default: 0
-    t.integer "skills_count", default: 0
-    t.index ["user_id"], name: "index_skills_on_user_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.bigint "skill_id"
-    t.string "name"
-    t.boolean "complete"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "resource_id"
-    t.index ["resource_id"], name: "index_tasks_on_resource_id"
-    t.index ["skill_id"], name: "index_tasks_on_skill_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -71,27 +24,10 @@ ActiveRecord::Schema.define(version: 20170925112539) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.integer "skills_count", default: 0
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "notes", "resources"
-  add_foreign_key "resources", "skills"
-  add_foreign_key "skills", "skills", column: "parent_id"
-  add_foreign_key "skills", "users"
-  add_foreign_key "tasks", "resources"
-  add_foreign_key "tasks", "skills"
 end
