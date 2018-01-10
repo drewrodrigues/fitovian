59bf2ed6f8d0373ad79a1e02db27c98874019d4b
Andrew <rodandrew95@gmail.com>
2018-01-09 17:51:59 -0800

    merge mikes work and cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery3
-//= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
 
   # pages
   root 'pages#landing'
+  get 'dashboard', to: 'pages#dashboard'
   get 'panel', to: 'pages#panel'
+  get 'choose-us', to: 'pages#choose_us'
+  get 'rates', to: 'pages#rates'
+  get 'services', to: 'pages#services'
+  get 'get-started', to: 'pages#get_started'
+  get 'about', to: 'pages#about'
+  get 'our-program', to: 'pages#our_program'
+  get 'contact-us', to: 'pages#contact_us'
 
   # lessons
   resources :lessons
