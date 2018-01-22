fe9f7300d977270241981fc44b4d1eb861e9e9b4
Drew Rodrigues <thesimpledev@gmail.com>
2018-01-22 08:56:29 -0800

    add current period end to users table

---
+++ db/migrate/20180122165347_add_subscription_time_stamps_to_user.rb
@@ -0,0 +1,5 @@
+class AddSubscriptionTimeStampsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :current_period_end, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122163426) do
+ActiveRecord::Schema.define(version: 20180122165347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180122163426) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.boolean "admin", default: false
+    t.date "current_period_end"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
