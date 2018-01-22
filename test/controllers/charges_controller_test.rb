1184b8e92e36358f730ce55a5f9b6e12443cbdb8
Drew Rodrigues <thesimpledev@gmail.com>
2018-01-22 08:31:37 -0800

    get basic test charge to work

---
+++ test/controllers/charges_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ChargesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get charges_new_url
+    assert_response :success
+  end
+
+end
