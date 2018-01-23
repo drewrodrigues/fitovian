53c18ae398b2da8d27c3781dac796bc033564f6b
Andrew <rodandrew95@gmail.com>
2018-01-23 11:55:45 -0800

    use layout for registration & login

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   # users
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
   authenticated :user do
