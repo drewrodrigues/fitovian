3715d83420445ba354b066ac1277e3ca32fb8e23
Andrew <rodandrew95@gmail.com>
2018-03-01 06:23:21 -0800

    add stripe mock to gemfile
    add model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,72 +1,158 @@
-class User < ApplicationRecord
-  # Schema ############################
-  # active_subscription: boolean
-  # - default: false
-  # admin: boolean
-  # - default: false
-  # current_period_end: date
-  # email: string
-  # stripe_customer_id: string
-  # stripe_subscription_id: string
+# id                        integer       not null, unique
+# active                    boolean       default(false)
+# admin                     boolean       default(false)
+# current_period_end        date          default(previous day)
+# email                     string        not null, unique
+# name                      string        not null
+# stripe_customer_id        string        not null, unique
+# stripe_subscription_id    string
 
+class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
-  # Questions #########################
-  def subscription_active?
-    self.active_subscription
-  end
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :stripe_customer_id, unless: :admin
+  validates_uniqueness_of :stripe_customer_id
 
-  def admin?
-    self.admin
+  scope :admins, -> { where('admin = true').order('name asc') }
+  scope :active_members, -> { where('active = true').order('name asc') }
+  scope :inactive_members, -> { where('active = false').order('name asc') }
+
+  def initialize(args = nil)
+    super(args)
+    set_stripe_customer_id unless is_admin?
   end
 
   def membership_active?
     return false unless current_period_end
-    self.current_period_end > Date.current
+    self.current_period_end >= Date.current
   end
 
-  # Actions ###########################
-  def cancel_subscription
-    subscription = Stripe::Subscription.retrieve(self.stripe_subscription_id)
-    subscription.delete(at_period_end: true)
-    self.active_subscription = false
+  def is_admin?
+    self.admin
+  end
+
+  def set_end_date(event)
+    if event.respond_to?(:current_period_end)
+      # Upon stripe subscription object
+      sub_end = Time.at(event.current_period_end).to_datetime
+    else
+      # Webhook object returned by Stripe on invoice.payment_succeeded
+      sub_end = Time.at(event.data.object.period_end).to_datetime
+    end
+
+    self.current_period_end = Date.new(sub_end.year, sub_end.month, sub_end.day)
     self.save
   end
 
   def subscribe
-    subscription = Stripe::Subscription.create({
+    # TODO: make error more descriptive
+    throw StandardError if subscribed?
+    re_activate if stripe_subscription && !active
+
+    @stripe_subscription = Stripe::Subscription.create({
       customer: self.stripe_customer_id,
-      items: [{plan: 'basic'}] # TODO: make this more flexible
+      items: [{plan: stripe_plan.id}]
     })
-    set_subscription_one_month
-    self.stripe_subscription_id = subscription.id
-    self.active_subscription = true
+
+    set_end_date(@stripe_subscription)
+    set_active
+
+    self.stripe_subscription_id = @stripe_subscription.id
     self.save
+  end
+
+  def cancel
+    stripe_subscription.delete(at_period_end: true)
+
+    self.active = false
+    self.save
+  end
+
+  def add_payment_method(token)
+    new_payment_method = stripe_customer.sources.create({source: token})
+    stripe_customer.default_source = new_payment_method.id
+
+    # REVIEW: doesn't this need a writer method
+    @default_payment_method = new_payment_method
   end
 
   def re_activate
-    subscription = Stripe::Subscription.retrieve(self.stripe_subscription_id)
-    subscription.items = [{
-        id: subscription.items.data[0].id,
-        plan: 'basic',
+    # if membership_active? returns false, then the subscription should be
+    # have been deleted already
+    # TODO: make error more descriptive
+    throw StandardError unless membership_active?
+
+    stripe_subscription.items = [{
+        id: stripe_subscription.items.data[0].id,
+        plan: stripe_plan.id,
     }]
-    subscription.save
-    self.active_subscription = true
+    stripe_subscription.save
+
+    self.active = true
     self.save
+    # REVIEW: do I need to call .save each time?
   end
 
-  def set_stripe_customer_id(stripeToken)
-    self.stripe_customer_id = Stripe::Customer.create(
-      source: stripeToken
-    ).id
+  def default_payment_method
+    return unless stripe_customer
+    @default_payment_method ||= stripe_customer.sources.retrieve(stripe_customer.default_source)
   end
 
-  def set_subscription_one_month
-    date = Date.current
-    next_month = (1..12).include?(date.month) ? date.month + 1 : 1
-    one_month_out = Date.new(date.year, next_month, date.day)
-    self.current_period_end = one_month_out
-    self.save
+  def payment_methods
+    return unless stripe_customer
+    stripe_customer.sources.list.data
   end
+
+  def subscribed?
+    stripe_subscription && stripe_subscription_active?
+  end
+
+  private
+    def set_stripe_customer_id
+      begin
+        self.stripe_customer_id = Stripe::Customer.create.id
+      rescue
+        self.stripe_customer_id = nil
+      end
+    end
+
+    def set_default_current_period_end
+      self.current_period_end = Date.today - 1
+    end
+
+    def stripe_customer
+      return unless self.stripe_customer_id
+      begin
+        @stripe_customer ||= Stripe::Customer.retrieve(self.stripe_customer_id)
+      rescue
+        @stripe_customer = nil
+      end
+    end
+
+    def stripe_subscription
+      return unless self.stripe_subscription_id
+      begin
+        @stripe_subscription ||= Stripe::Subscription.retrieve(self.stripe_subscription_id)
+      rescue
+        @stripe_subscription ||= nil
+      end
+    end
+
+    def stripe_subscription_active?
+      stripe_subscription.status == 'active'
+    end
+
+    def stripe_plan
+      @stripe_plan ||= Stripe::Plan.retrieve('basic')
+    end
+
+    def set_active
+      throw StandardError unless membership_active?
+
+      self.active = true
+      self.save
+    end
 end
