8419adc9390faeb6d7a2aa6979b25f90dee16e14
Drew Rodrigues <thesimpledev@gmail.com>
2018-03-09 16:20:12 -0800

    change support@wcwlc.com to noreply@wcwlc.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'wcwlc.com',
+    domain: 'gmail.com',
     user_name: ENV['wcwlc_email_username'],
     password: ENV['wcwlc_email_password'],
     authentication: :plain,
