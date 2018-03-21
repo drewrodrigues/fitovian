e1978f8a7ab0e3ac8eac8496dd201ac80dda2afe
Drew Rodrigues <thesimpledev@gmail.com>
2018-03-20 17:02:04 -0700

    remove test folder

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rspec-rails'
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- test/system/lessons_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class LessonsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit lessons_url
-  #
-  #   assert_selector "h1", text: "Lesson"
-  # end
-end
