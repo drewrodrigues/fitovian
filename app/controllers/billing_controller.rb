b4b7b135328692d35520b56b24fe396be49acba1
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-10 08:35:55 -0700

    update billing controller and views

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,22 +1,9 @@
 class BillingController < ApplicationController
-  # skip_before_action :require_payment_method!, on: [:new, :create]
-
   def new; end
 
-  def create
-    current_user.add_payment_method(params[:stripeToken])
-    current_user.subscribe
-    redirect_to billing_path, flash: {
-      success: 'Successfully subscribed'
-    }
-  rescue Stripe::StripeError, RuntimeError => e
-    flash.now[:alert] = e.message
-    redirect_to billing_path
+  def dashboard
+    @cards = current_user.cards
   end
-
-  def update; end
-
-  def dashboard; end
 
   def subscribe
     if current_user.re_activate || current_user.subscribe
