5356ffae0013e2eeccaf3a48aa3b85f1a9f3639f
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-10 08:50:32 -0700

    update factories

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do  
   factory :card do
-    user
-    stripe_id 'something'
+    stripe_id 'fake_stripe_id'
     default false
     last4 { rand(1000..9999) }
+    user
 
     factory :card_default do
       default true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,28 +4,10 @@ FactoryBot.define do
     email 'drew@example.com'
     name 'Drew'
     password 'password'
+    stripe_id nil
 
     factory :admin do
       admin true
-    end
-
-    factory :user_with_plan do
-      after(:create) do |user|
-        create_list(:plan, 1, user: user)
-      end
-    end
-
-    factory :user_with_card do
-      after(:create) do |user|
-        create_list(:card_default, 1, user: user)
-      end
-    end
-
-    factory :user_with_cards do
-      after(:create) do |user|
-        create_list(:card, 3, user: user)
-        create_list(:card_default, 1, user: user)
-      end
     end
   end
 end
