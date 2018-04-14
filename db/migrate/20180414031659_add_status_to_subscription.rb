2d87776008f67bbb65f9cd4ca3c9a80a20af2788
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-13 20:17:46 -0700

    add status to subscription

---
+++ db/migrate/20180414031659_add_status_to_subscription.rb
@@ -0,0 +1,5 @@
+class AddStatusToSubscription < ActiveRecord::Migration[5.1]
+  def change
+    add_column :subscriptions, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412150843) do
+ActiveRecord::Schema.define(version: 20180414031659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180412150843) do
     t.datetime "updated_at", null: false
     t.date "current_period_end", null: false
     t.boolean "active", default: false, null: false
+    t.string "status"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
