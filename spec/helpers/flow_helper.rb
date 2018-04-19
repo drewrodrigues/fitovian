9e6d03a20e37feb05f3a4acffe03a3b124e70baa
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-19 13:37:35 -0700

    pull out add_card helper

--- spec/helpers/flow_helper.rb
+++ spec/helpers/flow_helper.rb
@@ -5,4 +5,13 @@ module FlowHelper
     fill_in 'Password', with: user.password
     click_button 'Sign in'
   end
+
+  def add_card
+    within_frame 0 do
+      '5555555555554444'.split('').each { |c| find_field('Card number').native.send_keys(c) }
+      '5555'.split('').each { |c| find_field('MM / YY').native.send_keys(c) }
+      '555'.split('').each { |c| find_field('CVC').native.send_keys(c) }
+      '55555'.split('').each { |c| find_field('ZIP').native.send_keys(c) }
+    end
+  end
 end
