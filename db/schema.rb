7e61bf56d610e7077b1b4e972da9c77eba5fc754
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-21 16:53:49 -0700

    add category model tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420230117) do
+ActiveRecord::Schema.define(version: 20180421234648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20180420230117) do
     t.string "last4", null: false
     t.boolean "default", default: false, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:stacks) }
+  end
+
+  describe 'database columns' do
+    it { is_expected.to have_db_column(:title) }
+  end
+
+  describe 'factories' do
+    it 'has a valid base factory' do
+      expect(build_stubbed(:category)).to be_valid
+    end
+  end
 end
