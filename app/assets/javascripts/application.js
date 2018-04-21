73af66a92af3f8b5592c8e8b633a1054301c679f
Drew Rodrigues <thesimpledev@gmail.com>
2018-04-20 17:41:04 -0700

    make turbolinks play nice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require tinymce-jquery
-//= require rails-ujs
 //= require turbolinks
 //= require_tree .
 
