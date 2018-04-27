ba98125a7a70fdb7b5a568338dc58ec14b1e33c3
Andrwe <rodandrew95@gmail.com>
2018-04-26 17:13:25 -0700

    allow local image upload

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -69,6 +69,6 @@ class StacksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def stack_params
-      params.require(:stack).permit(:title, :category_id)
+      params.require(:stack).permit(:title, :category_id, :icon)
     end
 end
