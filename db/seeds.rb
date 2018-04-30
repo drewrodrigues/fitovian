43b3849b52760286ccd8da6d5c9d6134d9185735
Andrwe <rodandrew95@gmail.com>
2018-04-29 20:34:11 -0700

    make basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+
+6.times do 
+  Category.create!(
+    title: Faker::Book.genre,
+    color: Faker::Color.hex_color
+  )
+end
+
+30.times do
+  Stack.create!(
+    title: Faker::Beer.style,
+    category_id: Category.pluck(:id).sample,
+    summary: Faker::Lorem.paragraphs(1).first
+  )
+end
+
+300.times do
+  Lesson.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraphs(5).join(' '),
+    stack_id: Stack.pluck(:id).sample,
+    position: rand(1..100)
+  )
+end
