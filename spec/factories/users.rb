93a511b1092437155a4c267b32c25450572a73ce
Andrwe <rodandrew95@gmail.com>
2018-05-01 18:52:55 -0700

    reduce duplicated code with user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,6 @@
+require_relative '../helpers/cards_helper.rb'
+include CardsHelper
+
 FactoryBot.define do
   factory :user do
     admin false
@@ -5,6 +8,25 @@ FactoryBot.define do
     name 'Drew'
     password 'password'
     stripe_id nil
+
+    trait :with_plan do
+      after(:create) { |u| u.select_starter_plan }
+    end
+
+    trait :with_plan_and_card do
+      after(:create) do |u|
+        u.select_starter_plan
+        add_fake_card(u)
+      end
+    end
+
+    trait :onboarded do
+      after(:create) do |u|
+        u.select_starter_plan
+        add_fake_card(u)
+        u.subscribe
+      end
+    end
 
     factory :admin do
       admin true
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -22,126 +22,107 @@ RSpec.describe Subscription, type: :model do
 
   describe '#subscribe' do
     context 'user doesn\'t have a subscription' do
-      before(:each) do
-        @user = create(:user)
-        @user.select_starter_plan
-        add_fake_card(@user)
-        @user.subscribe
-      end
+      let(:user) { create(:user, :with_plan_and_card) }
+
+      before(:each) { user.subscribe }
 
       it 'should add the subscription to the user' do
-        expect(@user.subscription).to_not be_nil
+        expect(user.subscription).to_not be_nil
       end
 
       it 'should subscribe the user to the plan' do
-        stripe_subscription = @user.subscription.stripe_subscription
-        expect(stripe_subscription.customer).to eq(@user.stripe_id)
+        stripe_subscription = user.subscription.stripe_subscription
+        expect(stripe_subscription.customer).to eq(user.stripe_id)
       end
 
       it 'should set the user\'s subscription to active' do
-        expect(@user.subscription.active).to be true
+        expect(user.subscription.active).to be true
       end
 
       it 'should set the user\'s Stripe subscription status to active' do
-        subscription = @user.subscription.stripe_subscription
+        subscription = user.subscription.stripe_subscription
         expect(subscription.status).to eq('active')
       end
 
       it 'should set the current_period_end 1 month out' do
-        subscription = @user.subscription
+        subscription = user.subscription
         expect(subscription.current_period_end).to eq(Time.zone.today + 1.month)
       end
 
       it 'should have 1 stripe subscription' do
-        subscription_count = Stripe::Customer.retrieve(@user.stripe_id).subscriptions.total_count
+        subscription_count = Stripe::Customer.retrieve(user.stripe_id).subscriptions.total_count
         expect(subscription_count).to eq(1)
       end
     end
 
     context 'user already has an active subscription' do
-      before(:each) do
-        @user = create(:user)
-        @user.select_starter_plan
-        add_fake_card(@user)
-        @user.subscribe
-        @user.subscribe
-      end
+      let(:user) { create(:user, :onboarded) }
+
+      before(:each) { user.subscribe }
 
       it 'should return true' do
-        expect(@user.subscription.subscribe).to be true
+        expect(user.subscription.subscribe).to be true
       end
 
       it 'should not add another subscription to the user' do
-        subscription_count = Stripe::Customer.retrieve(@user.stripe_id).subscriptions.total_count
+        subscription_count = Stripe::Customer.retrieve(user.stripe_id).subscriptions.total_count
         expect(subscription_count).to eq(1)
       end
     end
 
     context 'user has an in-active subscription' do
-      before(:each) do
-        @user = create(:user)
-        @user.select_starter_plan
-        add_fake_card(@user)
-        @user.subscribe
-        @user.cancel
-      end
+      let(:user) { create(:user, :onboarded) }
+
+      before(:each) { user.cancel }
 
       it 'should return true' do
-        expect(@user.subscription).to be_truthy
+        expect(user.subscription).to be_truthy
       end
 
       it 'should not add another subscription to the user' do
-        subscription_count = Stripe::Customer.retrieve(@user.stripe_id).subscriptions.total_count
+        subscription_count = Stripe::Customer.retrieve(user.stripe_id).subscriptions.total_count
         expect(subscription_count).to eq(1)
       end
     end
   end
 
   describe '#cancel' do
-    context 'user doesn\'t have a subscription' do
-      before(:each) do
-        @user = create(:user)
-        @user.select_starter_plan
-        add_fake_card(@user)
-        @user.subscribe
-        @user.cancel
-      end
+    context 'when user doesn\'t have a subscription' do
+      let(:user) { create(:user, :onboarded) }
+
+      before(:each) { user.cancel }
 
       it 'should set to cancel at period end' do
-        subscription = @user.subscription.stripe_subscription
+        subscription = user.subscription.stripe_subscription
         expect(subscription.cancel_at_period_end).to be true
       end
 
       it 'should set active to false' do
-        expect(@user.subscription.active).to be false
+        expect(user.subscription.active).to be false
       end
 
       it 'should set status to canceled' do
-        subscription = @user.subscription
+        subscription = user.subscription
         expect(subscription.status).to eq('canceled')
       end
     end
 
-    context 'user\'s subscription is already canceled' do
-      before(:each) do
-        @user = create(:user)
-        @user.select_starter_plan
-        add_fake_card(@user)
-        @user.subscribe
-        @user.cancel
-      end
+    context 'when user\'s subscription is already canceled' do
+      let(:user) { create(:user, :onboarded) }
+
+      before(:each) { user.cancel }
 
       it 'should set to cancel at period end' do
-        subscription = @user.subscription.stripe_subscription
+        subscription = user.subscription.stripe_subscription
         expect(subscription.cancel_at_period_end).to be true
       end
 
       it 'should set active to false' do
-        expect(@user.subscription.active).to be false
+        expect(user.subscription.active).to be false
       end
 
       it 'should set status to canceled' do
-        subscription = @user.subscription
+        subscription = user.subscription
         expect(subscription.status).to eq('canceled')
       end
     end
