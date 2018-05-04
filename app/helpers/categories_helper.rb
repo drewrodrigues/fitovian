4538591da8b7b046159f96ee813575914c62eec1
Andrwe <rodandrew95@gmail.com>
2018-05-04 10:43:57 -0700

    allow the journey stack

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,13 @@
 module CategoriesHelper
+  def library_intro_path
+    if current_user
+      'the_journey'
+    else
+      'library_intro'
+    end
+  end
+
+  def the_journey_path
+    Stack.where(title: 'The Journey').first
+  end
 end
