8f88f5e608082430cf0ba31e54b0f1374b90d4e7
Andrew <thesimpledev@gmail.com>
2018-05-10 20:45:25 -0700

    deliver later action mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,25 +4,25 @@ class PagesController < ApplicationController
   def landing; end
 
   def contact_us
-    if MessageMailer.contact_us(params[:body], params[:email], params[:name]).deliver
-      redirect_to landing_path, flash: {
+    if MessageMailer.contact_us(params[:body], params[:email], params[:name]).deliver_later
+      redirect_to root_path, flash: {
         success: 'Thanks for messaging us! We\'ll get back to you as soon as possible!'
       }
     else
-      redirect_to landing_path, flash: {
+      redirect_to root_path, flash: {
         alert: 'Oh no, something wen\'t wrong! Contact us directly at contact@wcwlc.com'
       }
     end
   end
 
   def updates
-    message = if MessageMailer.updates(params[:email]).deliver
+    message = if MessageMailer.updates(params[:email]).deliver_later
                 { success: 'Thanks for signing up for updates!' }
               else
                 { alert: 'Oh no, something wen\'t wrong! Contact us directly at contact@wcwlc.com' }
               end
     Lead.create(email: params[:email], context: 'wants to receive updates')
-    redirect_to landing_path, flash: message
+    redirect_to root_path, flash: message
   end
 
   def dashboard
