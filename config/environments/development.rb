7cc12be5848583eca8d2799a923bf5557741ba45
Andrew <thesimpledev@gmail.com>
2018-05-11 15:36:20 -0700

    remove bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,4 @@
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-  # Bullet.growl         = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -89,11 +80,4 @@ Rails.application.configure do
     url: ":s3_domain_url",
     path: "/:class/:id/:style/:filename"
   }
-
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-  end
 end
--- spec/factories/handlers_selections_handlers.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :handlers_selections_handler, class: 'Handlers::SelectionsHandler' do
-  end
-end
