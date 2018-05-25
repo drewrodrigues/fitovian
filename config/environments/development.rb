878b60d6665f4743bfc5ce19cf1072fef8d379bf
Andrew <thesimpledev@gmail.com>
2018-05-24 19:07:09 -0700

    remove query cache and asset log in logger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_view.logger = nil # remove assets in logger
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
